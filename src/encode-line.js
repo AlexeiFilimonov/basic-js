840491306f2f97d95be8c79b3c201126008c057a
AlexeiFilimonov <nlifeaf@mail.ru>
2023-04-17 09:03:38 +0300

    feat: add encode-line and file-names task solutions

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  const res = [];
+  let counter = 1;
+  for (let i=0;i<str.length;i++) {
+    if (str[i] !== str[i+1]) {
+      counter===1?res.push(str[i]):res.push(counter+str[i]);
+      counter = 1;
+    }
+    else counter++;
+  }
+  return res.join('');
 }
 
 module.exports = {
